26c84065cb4ed40354389363291c4566ffbaa3d6
SRD4RW1N-code <daportillor@ufpso.edu.co>
2025-03-01 07:20:14 -0500

    ejercicio de I/O

---
+++ src/ejercicio.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ejercicio {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = input.next();
+        System.out.print("Ingrese su edad: ");
+        byte edad = input.nextByte();
+
+        System.out.println("Hola, " + nombre + ". Tienes " + edad + " años.");  
+        input.close(); 
+    }
+}
