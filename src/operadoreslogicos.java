bae396872e407dfc105c8d304855bf6b6cc6dc03
andres <aarrietameneses@gmail.com>
2025-03-07 07:47:02 -0500

    se agrego el ejercicio de operadores logicos

---
+++ src/operadoreslogicos.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class operadoreslogicos {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("ingrese el numero a");
+        int a = input.nextInt();
+        System.out.println("ingrese el numero b");
+        int b = input.nextInt();
+        System.out.println("ingrese el numero c");
+        int c = input.nextInt();
+        System.out.println("a > b && b < c: " + (a > b && b < c)); // true
+        System.out.println("a < b || b > c: " + (a < b || b > c)); // false
+        System.out.println("!(a > b): " + !(a > b)); // false
+    }
+}
