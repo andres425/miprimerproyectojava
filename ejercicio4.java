9ae1d0d8ab92a4ad935471ef039f2dd403185f47
andres <aarrietameneses@gmail.com>
2025-03-07 11:54:36 -0500

    se agrego el ejercicio 4

---
+++ ejercicio4.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ejercicio4{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("ingrese un numero");
+        int numero = input.nextInt();
+        int factorial = 1;
+        int i = 1;
+        while (i <= numero) {
+            factorial *= i;
+            i++;
+        }
+        System.out.println("El factorial de " + numero + " es " + factorial);
+    }
+}
