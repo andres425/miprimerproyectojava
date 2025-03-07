4147c1da24aecdbf0e81b068e7d12dcb6bb8df8e
andres <aarrietameneses@gmail.com>
2025-03-07 12:00:11 -0500

    se agrego el ejercicio 5

---
+++ ejercicio5.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class ejercicio5 {
+public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
+    System.out.println("ingrese la tabla de multiplicar que desea ver");
+    int tabla = input.nextInt();
+    for (int i= 1; i <= 10; i++) {
+      System.out.println(tabla + " x " + i + " = " + tabla * i);
+    }
+ }
+}
